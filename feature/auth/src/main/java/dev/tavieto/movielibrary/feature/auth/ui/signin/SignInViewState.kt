29e9939adfee1b8ba4d4289ddd12f66356699c95
Otávio Gabriel <tavieto@outlook.com>
2023-07-03 09:48:23 -0300

    chore: remove default credentials

--- feature/auth/src/main/java/dev/tavieto/movielibrary/feature/auth/ui/signin/SignInViewState.kt
+++ feature/auth/src/main/java/dev/tavieto/movielibrary/feature/auth/ui/signin/SignInViewState.kt
@@ -3,8 +3,8 @@ package dev.tavieto.movielibrary.feature.auth.ui.signin
 
 
 data class SignInViewState(
-    val email: String = "samilly@test.com",
-    val password: String = "123456",
+    val email: String = "",
+    val password: String = "",
     val isLoading: Boolean = false,
     val error: Throwable? = null
 )
